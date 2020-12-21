d970c58e6e08f1f8eda80405e4f45285e0a6b52b
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2020-12-21 17:48:08 +0200

    Jauks serveris

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hei! Yo!</h1>"
+
+@app.route("/json")
+def json():
+    f = open("dati.json", "r", encoding="utf-8")
+    return f.read()
+
+if __name__ == '__main__':
+    app.run(debug=True)
